6a5ff07892f5f5c4ef463b0c6a8108468b4d8748
liveseyy <dima74ermilov@gmail.com>
2021-01-04 15:36:39 +0500

    make post list and post detail pages

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
+from django.shortcuts import reverse
 
 
 class PublishedManager(models.Manager):
@@ -29,3 +30,7 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[
+            self.publish.year, self.publish.month, self.publish.day, self.slug])
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
+from .models import Post
 
-# Create your views here.
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(
+        Post, slug=post, status='published', publish__year=year, publish__month=month, publish__day=day,
+    )
+    context = {'post': post}
+    return render(request, 'blog/post/detail.html', context)
+
