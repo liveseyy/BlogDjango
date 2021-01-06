f69d82b72497a836ee5c639bfe45320f6564248f
liveseyy <dima74ermilov@gmail.com>
2021-01-06 11:24:53 +0500

    added a display latest and most commented posts

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,4 +1,6 @@
 from django import template
+from django.db.models import Count
+from django.db.models import Q
 from ..models import Post
 
 register = template.Library()
@@ -7,3 +9,15 @@ register = template.Library()
 @register.simple_tag
 def total_posts():
     return Post.published.count()
+
+
+@register.simple_tag
+def get_most_commented_posts(count=5):
+    return Post.published.annotate(
+        total_comments=Count('comments', filter=Q(comments__active=True))).order_by('-total_comments')[:count]
+
+
+@register.inclusion_tag('blog/post/latest_posts.html')
+def show_latest_posts(count=5):
+    latest_posts = Post.published.order_by('-publish')[:count]
+    return {'latest_posts': latest_posts}
