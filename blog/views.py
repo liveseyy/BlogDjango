d9a574198efab5d2198d7375ff0b10ea431f2d25
liveseyy <dima74ermilov@gmail.com>
2021-01-07 19:28:59 +0500

    changed search: add postgres SearchQuery and SearchRank

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from django.core.mail import send_mail
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
 from django.db.models import Count
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchRank, SearchVector, SearchQuery
 
 from .models import Post
 from taggit.models import Tag
@@ -85,7 +85,10 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
-            results = Post.objects.annotate(search=SearchVector('title', 'body')).filter(search=query)
+            search_vector = SearchVector('title', 'body')
+            search_query = SearchQuery(query)
+            results = Post.published.annotate(search=search_vector, rank=SearchRank(search_vector, search_query))\
+                .filter(search=search_query).order_by('-rank')
 
     context = {'form': form, 'query': query, 'results': results}
     return render(request, 'blog/post/search.html', context)
